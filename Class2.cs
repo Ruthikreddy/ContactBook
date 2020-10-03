baee7e6a552ef8c41c0e1d934a2fd6785a89074a
Ruthikreddy <ruthikreddy15@gmail.com>
2020-10-03 19:41:38 +0530

    [adding contatct details]

---
+++ Class2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class AddressBook
+{
+    List<Address> addresses;
+
+    public AddressBook()
+    {
+        addresses = new List<Address>();
+    }
+
+    public bool add(string Firstname, string Lastname, string address, string city, string state, int zip, double phoneNo, string email)
+    {
+        Address addr = new Address(Firstname, Lastname, address, city, state, zip, phoneNo, email);
+        Address result = find(Firstname);
+
+        if (result == null)
+        {
+            addresses.Add(addr);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //public bool remove(string Firstname)
+    //{
+    //    Address addr = find(Firstname);
+
+    //    if (addr != null)
+    //    {
+    //        addresses.Remove(addr);
+    //        return true;
+    //    }
+    //    else
+    //    {
+    //        return false;
+    //    }
+    //}
+
+    //public void list(Action<Address> action)
+    //{
+    //    addresses.ForEach(action);
+    //}
+
+    public bool isEmpty()
+    {
+        return (addresses.Count == 0);
+    }
+
+    public Address find(string name)
+    {
+        Address addr = addresses.Find((a) => a.Firstname == name);
+        return addr;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+class AddressPrompt
+{
+    AddressBook book;
+
+    public AddressPrompt()
+    {
+        book = new AddressBook();
+    }
+
+    static void Main(string[] args)
+    {
+        string selection = "";
+        AddressPrompt prompt = new AddressPrompt();
+
+        prompt.displayMenu();
+        while (!selection.ToUpper().Equals("Q"))
+        {
+            Console.WriteLine("Selection: ");
+            selection = Console.ReadLine();
+            prompt.performAction(selection);
+        }
+    }
+
+    void displayMenu()
+    {
+        Console.WriteLine("Main Menu");
+        Console.WriteLine("=========");
+        Console.WriteLine("A - Add an Address");
+          }
+
+    void performAction(string selection)
+    {
+        string Firstname = "";
+        string lastName = "";
+        string address = "";
+        string city = "";
+        string state = "";
+        int zip = 0;
+        double phoneNo = 0;
+        string eMail = "";
+        switch (selection.ToUpper())
+        {
+            case "A":
+                Console.WriteLine("Enter Name: ");
+                Firstname = Console.ReadLine();
+                lastName = Console.ReadLine();
+                address = Console.ReadLine();
+                city = Console.ReadLine();
+                state = Console.ReadLine();
+                zip = Convert.ToInt32(Console.ReadLine());
+                phoneNo = Convert.ToDouble(Console.ReadLine());
+                eMail = Console.ReadLine();
+
+
+                if (book.add(Firstname, lastName, address, city, state, zip, phoneNo, eMail))
+                {
+                    Console.WriteLine("Address successfully added!");
+                }
+                else
+                {
+                    Console.WriteLine("An address is already on file for {0}.", Firstname);
+                }
+                break;
+            
+        }
+    }
+}
